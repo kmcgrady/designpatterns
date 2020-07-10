bb67633c71236f42d5442df36d0e51143d2a163b
Ken McGrady <ken.mcgrady@gmail.com>
2020-07-10 11:27:04 -0700

    Initial Spring Project

---
+++ src/test/java/com/ga/designpatterns/DesignpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
