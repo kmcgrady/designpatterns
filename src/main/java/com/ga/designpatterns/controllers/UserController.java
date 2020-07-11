86936278d529a6ce95e18e79eb26d90a9af0553b
Ken McGrady <ken.mcgrady@gmail.com>
2020-07-10 19:20:24 -0700

    Web Application Working

---
+++ src/main/java/com/ga/designpatterns/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.ga.designpatterns.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+    @RequestMapping(value="")
+    @ResponseBody
+    public String index(){
+        return "Hello World";
+    }
+
+    @RequestMapping(value="goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye";
+    }
+}
